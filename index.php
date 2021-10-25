e56c10b1bf98bdaddeadc383a67707b20584d219
CHRINIMUE <chrinimue@posteo.de>
2021-10-25 18:18:25 +0200

    first api test

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php 
- echo("Ich bin Hallo 2");
-php?>
+    $lat = $_GET["lat"];
+    $lng = $_GET["lng"];
+    $radius = $_GET["radius"];
+    $sort = $_GET["sort"];
+    $type = $_GET["type"];
+    $json = file_get_contents('https://creativecommons.tankerkoenig.de/json/list.php'
+        ."?lat=$lat"     // geographische Breite
+        ."&lng=$lng"     //               Länge
+        ."&rad=$radius"  // Suchradius in km
+        ."&sort=$sort"   // Sortierung: 'price' oder 'dist' - bei type=all diesen Parameter weglassen
+        ."&type=$type"   // Spritsorte: 'e5', 'e10', 'diesel' oder 'all'
+        ."&apikey=00000000-0000-0000-0000-000000000002");   // Demo-Key ersetzen!
+    $data = json_decode($json);
+    display($data);
+?>
