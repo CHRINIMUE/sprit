8d81a2236fefff4124d2cc065de824d0a1f9794e
Manuel Weber <m.weber-1999@hotmail.de>
2021-10-25 19:09:40 +0200

    t

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     $lat = isset($_GET["lat"]) ? $_GET["lat"] : '47.937580';
     $lng = isset($_GET["lng"]) ? $_GET["lng"] : '10.235170';
     $radius = isset($_GET["price"]) ? $_GET["price"] : '10';
-    $sort = isset($_GET["sort"]) ? $_GET["sort"] 'price';
+    $sort = isset($_GET["sort"]) ? $_GET["sort"] : 'price';
     $type = isset($_GET["type"]) ? $_GET["type"] : 'e5';
 
     echo("Got request with: " .$lat . ", " . $lng . ", " . $radius . ", " . $sort . ", " . $type);
